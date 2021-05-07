7350907ad086ef07a51b5c6beed01eeecdbebaec
DanilBocharnikov <dbocharnikov92@gmail.com>
2021-05-07 03:10:00 +0300

    configure test extension and building all projects

--- FuzzyInferenceSystem.Tests/UnitTest1.cs
+++ FuzzyInferenceSystem.Tests/UnitTest1.cs
@@ -3,12 +3,11 @@ using Xunit;
 
 namespace FuzzyInferenceSystem.Tests
 {
-    public class UnitTest1
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
     {
-        [Fact]
-        public void Test1()
-        {
-
-        }
     }
+  }
 }
