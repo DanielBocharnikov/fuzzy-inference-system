425ef2b30544ece5f5d969bc02970071f1086286
Даниил Бочарников <dbocharnikov92@gmail.com>
2021-05-11 14:47:06 +0300

    add seedwork project with entity&value object implementation

---
+++ FuzzyInferenceSystem.SeedWork/Entity.cs
@@ -0,0 +1,47 @@
+﻿namespace FuzzyInferenceSystem.SeedWork {
+  public abstract class Entity<TId> where TId : ValueObject {
+    private int? _requestedHashCode;
+
+    public TId Id { get; protected set; }
+
+    public bool IsTransient() => Id is default(TId);
+
+    public override bool Equals(object obj) {
+      if (obj is null || !(obj is Entity<TId>)) {
+        return false;
+      }
+
+      if (GetType() != obj.GetType()) {
+        return false;
+      }
+
+      if (ReferenceEquals(this, obj)) {
+        return true;
+      }
+
+      if ((obj as Entity<TId>)?.IsTransient() == true || IsTransient()) {
+        return false;
+      } else {
+        return (obj as Entity<TId>)?.Id == Id;
+      }
+    }
+
+    public override int GetHashCode() {
+      if (!IsTransient()) {
+        if (!_requestedHashCode.HasValue) {
+          _requestedHashCode = Id.GetHashCode() ^ 31;
+        }
+
+        return _requestedHashCode.Value;
+      } else {
+        return base.GetHashCode();
+      }
+    }
+
+    public static bool operator ==(Entity<TId> left, Entity<TId> right)
+      => (left is null) ? right is null : left.Equals(right);
+
+    public static bool operator !=(Entity<TId> left, Entity<TId> right)
+      => !(left == right);
+  }
+}
---
+++ FuzzyInferenceSystem.SeedWork/ValueObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FuzzyInferenceSystem.SeedWork {
+  public abstract class ValueObject {
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object obj) {
+      if (obj is null || GetType() != obj.GetType()) {
+        return false;
+      }
+
+      return GetEqualityComponents().SequenceEqual((obj as ValueObject)?.GetEqualityComponents());
+    }
+
+    public override int GetHashCode() => GetEqualityComponents()
+      .Select(x => (x?.GetHashCode()) ?? 0)
+      .Aggregate((x, y) => x ^ y);
+
+    public static bool operator ==(ValueObject left, ValueObject right) {
+      if (left is null ^ right is null) {
+        return false;
+      }
+
+      return left?.Equals(right) != false;
+    }
+
+    public static bool operator !=(ValueObject left, ValueObject right) => !(left == right);
+
+    public ValueObject GetCopy() => MemberwiseClone() as ValueObject;
+  }
+}
