8c463f02f057ace53fb269caf54945f61cfae60f
Даниил Бочарников <dbocharnikov92@gmail.com>
2021-05-13 16:45:30 +0300

    Add Fuzzy Set

--- FuzzyInferenceSystem.Domain/FuzzySet.cs
+++ FuzzyInferenceSystem.Domain/FuzzySet.cs
@@ -1,7 +1,21 @@
-﻿namespace FuzzyInferenceSystem.Domain
-{
-  class FuzzySet
-  {
+﻿using System;
+using System.Collections.Generic;
 
+namespace FuzzyInferenceSystem.Domain
+{
+  public class FuzzySet<TValue>
+  {
+    private readonly List<TValue> _domain = new();
+    private readonly List<double> _degreesOfMembership = new();
+
+    public Guid Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public string Description { get; private set; }
+
+    public IEnumerable<TValue> Domain => _domain;
+
+    public IEnumerable<double> DegreesOfMembership => _degreesOfMembership;
   }
 }
